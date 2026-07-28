ebb979d13e8695e91c9519a781cd94ab58463747
gauri 917540 <gauri437108@gmail.com>
2026-07-28 07:32:09 -0600

    Add decision tree model for student dropout prediction

--- model.py
+++ model.py
@@ -1,1 +1,10 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
 
+df = pd.read_csv("student_data.csv")
+
+X = df[["Attendance","Marks","StudyHours"]]
+y = df["Dropout"]
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
